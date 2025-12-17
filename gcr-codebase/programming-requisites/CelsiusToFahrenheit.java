983ccb25b9571c77e894d3c71e2419aead7ec64d
PSuhani2404 <suhani.pawar04@gmail.com>
2025-12-17 09:53:30 +0530

    [Suhani] : Added CelsiusToFahrenhiet

---
+++ gcr-codebase/programming-requisites/CelsiusToFahrenheit.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class CelsiusToFahrenheit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+		double celsius = scanner.nextDouble();
+		double fahrenheit = (celsius * 9.0 / 5.0) + 32;
+		System.out.println(fahrenheit);
+		scanner.close();
+    }
+}
