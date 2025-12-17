f545ab33396afcf2bebe1cf0eec8d3f8305b4290
PSuhani2404 <suhani.pawar04@gmail.com>
2025-12-17 16:26:55 +0530

    [Suhani] : Added KilometersToMiles program

---
+++ gcr-codebase/programming-elements/Level1/KilometersToMiles.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class KilometersToMiles{
+	public static void main(String[] args){
+	//Creating a variable named as kilometer and assigning value to it
+	double kilometer = 10.8;
+	
+	//Calculation
+	double miles = kilometer * 1.6;
+	
+	//Display result
+	System.out.println(" The distance " + kilometer + " km in miles is " + miles);
+	}
+}
