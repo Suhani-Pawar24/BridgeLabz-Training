0f6bce0440a5102b3f963e12689669b608b30517
PSuhani2404 <suhani.pawar04@gmail.com>
2025-12-22 08:00:38 +0530

    [Suhani] : added QuadraticCheck program

---
+++ gcr-codebase/Methods/Level2/Quadratic.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+public class Quadratic
+{
+
+    // Method to find roots of quadratic equation
+    public static double[] findRoots(double a, double b, double c) 
+	{
+        double delta = Math.pow(b, 2) - 4 * a * c;
+
+        if (delta > 0)
+		{
+            double root1 = (-b + Math.sqrt(delta)) / (2 * a);
+            double root2 = (-b - Math.sqrt(delta)) / (2 * a);
+            return new double[]{root1, root2};
+        } 
+        else if (delta == 0)
+		{
+            double root = -b / (2 * a);
+            return new double[]{root};
+        } 
+        else
+		{
+            return new double[0]; // no real roots
+        }
+    }
+
+    public static void main(String[] args)
+	{
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a: ");
+        double a = input.nextDouble();
+        System.out.print("Enter b: ");
+        double b = input.nextDouble();
+        System.out.print("Enter c: ");
+        double c = input.nextDouble();
+
+        double[] roots = findRoots(a, b, c);
+
+        if (roots.length == 2)
+		{
+            System.out.println("Two roots: " + roots[0] + " and " + roots[1]);
+        } 
+        else if (roots.length == 1)
+		{
+            System.out.println("One root: " + roots[0]);
+        } 
+        else
+		{
+            System.out.println("No real roots");
+        }
+
+        input.close();
+    }
+}
