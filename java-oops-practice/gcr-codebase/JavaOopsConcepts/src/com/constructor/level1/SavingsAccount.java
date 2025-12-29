617c14c972b1a2f747661ebcac832957277e0a89
PSuhani2404 <suhani.pawar04@gmail.com>
2025-12-29 18:45:42 +0530

    [Suhani] : added subclass of Bank Account problem

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/SavingsAccount.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class SavingsAccount extends BankAccount
+{
+	public SavingsAccount(int accountNumber, String accountHolder, double balance)
+	{
+		super(accountNumber,accountHolder,balance);
+	}
+
+	public void displayDetails()
+	{
+		System.out.println("Account number: " + accountNumber);
+		System.out.println("Account holder: " + accountHolder);
+		System.out.println("Balance : " + getBalance());
+	}
+
+	public static void main(String[] args)
+	{
+		SavingsAccount obj = new SavingsAccount(1234, "Aditya", 30000);
+		obj.displayDetails();
+		
+		obj.setBalance(50000);
+		System.out.println("Updated Balance: " + obj.getBalance());
+	}
+}	
