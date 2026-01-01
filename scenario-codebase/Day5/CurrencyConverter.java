33752243183be8cc2dd94b37ce109ec494a645f5
PSuhani2404 <suhani.pawar04@gmail.com>
2026-01-01 11:26:56 +0530

    [Suhani] : added Currency Converter program

---
+++ scenario-codebase/Day5/CurrencyConverter.java
@@ -0,0 +1,43 @@
+/*18. Currency Exchange Kiosk 💱
+Design a currency converter:
+● Take INR amount and target currency.
+● Use a switch to apply the correct rate.
+● Ask if the user wants another conversion (do-while).*/
+import java.util.*;
+public class CurrencyConverter
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        char choice;
+
+        do
+        {
+            System.out.print("Enter amount in INR: ");
+            double inr = sc.nextDouble();
+
+            System.out.println("Choose currency:");
+            System.out.println("1. USD");
+            System.out.println("2. EUR");
+            System.out.println("3. GBP");
+
+            int option = sc.nextInt();
+            double result = 0;
+
+            switch(option)
+            {
+                case 1: result = inr * 0.012; break;
+                case 2: result = inr * 0.011; break;
+                case 3: result = inr * 0.0095; break;
+                default:
+                    System.out.println("Invalid currency");
+            }
+
+            System.out.println("Converted amount: " + result);
+
+            System.out.print("Do you want another conversion? (y/n): ");
+            choice = sc.next().charAt(0);
+
+        } while(choice == 'y' || choice == 'Y');
+    }
+}
