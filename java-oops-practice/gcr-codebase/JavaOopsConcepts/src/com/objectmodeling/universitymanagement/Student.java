646e7cde64d0cef9c8c423eb1c59c391494af77c
PSuhani2404 <suhani.pawar04@gmail.com>
2026-01-02 16:05:14 +0530

    [Suhani] : added university management program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/universitymanagement/Student.java
@@ -0,0 +1,31 @@
+package com.objectmodeling.universitymanagement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student 
+{
+		private int studentId;
+	    private String name;
+	    private List<Course> enrolledCourses = new ArrayList<>();
+
+	    public Student(int studentId, String name)
+	    {
+	        this.studentId = studentId;
+	        this.name = name;
+	    }
+
+	   
+	    public void enrollCourse(Course course) 
+	    {
+	        enrolledCourses.add(course);
+	        course.addStudent(this);
+	        System.out.println(name + " enrolled in " + course.getCourseName());
+	    }
+
+	    public String getName() 
+	    {
+	        return name;
+	    }
+}
+
+
