8b6340180727b7946c7a6aecbd5886ba749bdb4f
PSuhani2404 <suhani.pawar04@gmail.com>
2026-01-02 14:38:26 +0530

    [Suhani] : added bank and account program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/Bank/Customer.java
@@ -0,0 +1,19 @@
+package com.objectmodeling.Bank;
+
+public class Customer {
+	    private String name;
+
+	    public Customer(String name) {
+	        this.name = name;
+	    }
+
+	    public void viewBalance(Account account) {
+	        System.out.println(name + "'s balance: ₹" + account.getBalance());
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+	}
+
+
