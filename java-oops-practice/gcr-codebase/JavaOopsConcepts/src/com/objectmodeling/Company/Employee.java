b4053b5462cdaa6d27ced7c17a26046b87452f57
PSuhani2404 <suhani.pawar04@gmail.com>
2026-01-02 14:34:17 +0530

    [Suhani] : added company and department program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/Company/Employee.java
@@ -0,0 +1,15 @@
+package com.objectmodeling.Company;
+
+public class Employee {
+	    private String name;
+
+	    public Employee(String name) {
+	        this.name = name;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+	}
+
+
