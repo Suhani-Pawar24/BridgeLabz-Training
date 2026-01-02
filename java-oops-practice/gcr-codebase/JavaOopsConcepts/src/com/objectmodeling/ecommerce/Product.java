0e4a00e9fa58618685fcd03730ea99453c80d705
PSuhani2404 <suhani.pawar04@gmail.com>
2026-01-02 15:48:18 +0530

    [Suhani] : added ecommerce program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/ecommerce/Product.java
@@ -0,0 +1,27 @@
+package com.objectmodeling.ecommerce;
+
+public class Product 
+{
+	    private int productId;
+	    private String productName;
+	    private double price;
+
+	    public Product(int productId, String productName, double price)
+	    {
+	        this.productId = productId;
+	        this.productName = productName;
+	        this.price = price;
+	    }
+
+	    public double getPrice()
+	    {
+	        return price;
+	    }
+
+	    public String getProductName()
+	    {
+	        return productName;
+	    }
+	}
+
+
