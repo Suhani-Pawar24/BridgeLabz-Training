f7d993bbf6e184adedc01408bb72f04b19f34aa2
PSuhani2404 <suhani.pawar04@gmail.com>
2026-01-08 10:30:06 +0530

    [Suhani] : added loan buddy program

---
+++ java-oops-practice/scenario-based/JavaOopsScenario/src/com/day5/loanbuddy/IApprovable.java
@@ -0,0 +1,10 @@
+package com.day5.loanbuddy;
+
+public interface IApprovable 
+{
+	
+		abstract void approveLoan();
+		abstract double calculateEMI();
+		
+}
+
---
+++ java-oops-practice/scenario-based/JavaOopsScenario/src/com/day5/loanbuddy/LoanBuddyApp.java
@@ -0,0 +1,22 @@
+package com.day5.loanbuddy;
+
+public class LoanBuddyApp 
+{
+	
+	    public static void main(String[] args) 
+	    {
+
+	        Applicant applicant1 = new Applicant("Pratishtha", 720, 60000, 800000);
+
+	        LoanApplication loan = new HomeLoan(applicant1, 240);
+
+	        loan.approveLoan();
+
+	        if (loan.isApproved()) 
+	        {
+	            System.out.println("Monthly EMI: ₹" +
+	                Math.round(loan.calculateEMI()));
+	        }
+	    }
+}
+
