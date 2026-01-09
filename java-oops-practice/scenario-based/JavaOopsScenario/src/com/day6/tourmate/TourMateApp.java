f56364e191bf3f03dab957497b9bf7a28f710b95
PSuhani2404 <suhani.pawar04@gmail.com>
2026-01-09 15:17:37 +0530

    [Suhani] : modified tour app java file

--- java-oops-practice/scenario-based/JavaOopsScenario/src/com/day6/tourmate/TourMateApp.java
+++ java-oops-practice/scenario-based/JavaOopsScenario/src/com/day6/tourmate/TourMateApp.java
@@ -4,19 +4,19 @@ public class TourMateApp
 {
 	public static void main(String[] args) 
 	{
-		 // --- Setting up an International Trip (Paris) ---
-        Transport flight = new Transport(45000); // 10% tax internally added
-        Hotel resort = new Hotel(5000, 4);       // 5000 per night for 4 days
+		
+        Transport flight = new Transport(45000); 
+        Hotel resort = new Hotel(5000, 5);     
         Activity sightseeing = new Activity("Eiffel Tower Visit", 3000, 1000);
 
         InternationalTrip trip1 = new InternationalTrip("Paris", flight, resort, sightseeing);
         
-        trip1.displayPackage(); // Shows encapsulated budget calculation
-        trip1.book();           // Calls International booking logic
+        trip1.displayPackage();					
+        trip1.book();          
         
         System.out.println("------------------------------------");
 
-        // --- Setting up a Domestic Trip (Manali) ---
+     
         Transport bus = new Transport(2000);
         Hotel cottage = new Hotel(1500, 3);
         Activity trekking = new Activity("Solang Valley Trek", 500, 200);
