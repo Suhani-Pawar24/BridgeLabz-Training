42920ff97cd6c5481d2e996572945e7dd4616371
PSuhani2404 <suhani.pawar04@gmail.com>
2026-01-17 13:24:10 +0530

    [Suhani] : added book shelf program

---
+++ dsa-practice/scenario-based/DSAScenariobased/src/com/day2/bookshelf/Book.java
@@ -0,0 +1,19 @@
+package com.day2.bookshelf;
+
+public class Book 
+{
+	    String title;
+	    String author;
+
+	    Book(String title, String author) {
+	        this.title = title;
+	        this.author = author;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return title + " by " + author;
+	    }
+}
+
+
